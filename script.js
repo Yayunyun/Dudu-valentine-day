00f88dca1f96ae9c66dcd846460167459f1b65d9
Yayunyun <yayun@Yas-MacBook-Pro.local>
2026-02-13 21:25:21 -0700

    change message

--- script.js
+++ script.js
@@ -108,8 +108,8 @@ function handleNoClick() {
     const gifIndex = Math.min(noClickCount, gifStages.length - 1)
     swapGif(gifStages[gifIndex])
 
-    // Runaway starts at click 5
-    if (noClickCount >= 5 && !runawayEnabled) {
+    // Runaway starts at click 7
+    if (noClickCount >= 7 && !runawayEnabled) {
         enableRunaway()
         runawayEnabled = true
     }
