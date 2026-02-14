fd1d80671e9e813539db59eebd99d805bbe9f895
Yayunyun <yayun@Yas-MacBook-Pro.local>
2026-02-13 21:31:01 -0700

    change message

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const gifStages = [
     "https://media.tenor.com/EBV7OT7ACfwAAAAj/u-u-qua-qua-u-quaa.gif",    // 0 normal
-    "https://media1.tenor.com/m/uDugCXK4vI4AAAAd/chiikawa-hachiware.gif",  // 1 confused
-    "https://media.tenor.com/f_rkpJbH1s8AAAAj/somsom1012.gif",             // 2 pleading
-    "https://media.tenor.com/OGY9zdREsVAAAAAj/somsom1012.gif",             // 3 sad
-    "https://media1.tenor.com/m/WGfra-Y_Ke0AAAAd/chiikawa-sad.gif",       // 4 sadder
-    "https://media.tenor.com/CivArbX7NzQAAAAj/somsom1012.gif",             // 5 devastated
+    "gif/Willyoubemyvalentines.jpg",  // 1 confused
+    "gif/再給你一次機會.jpg",             // 2 pleading
+    "gif/嘟嘟會很難過.JPEG",             // 3 sad
+    "gif/Please.jpg",       // 4 sadder
+    "gif/最幸福的寶寶！.jpg",             // 5 devastated
     "https://media.tenor.com/5_tv1HquZlcAAAAj/chiikawa.gif",               // 6 very devastated
     "https://media1.tenor.com/m/uDugCXK4vI4AAAAC/chiikawa-hachiware.gif"  // 7 crying runaway
 ]
